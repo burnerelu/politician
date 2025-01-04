451d948868f1f290546348ad064b819c41a5a661
Burnerelu <Burnerelu@gmail.com>
2025-01-04 22:02:53 +0200

    Created sample init db script and docker container for test setup

---
+++ database/scripts/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE parties (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  abbrev VARCHAR(10) NOT NULL,
+  description VARCHAR(200)
+);
+
+CREATE TABLE politicians (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  party_id INT REFERENCES parties(id),
+  bio VARCHAR(200)
+);
+
+INSERT INTO parties (name, abbrev, description) VALUES ('National Party', 'NP', 'Left oriented');
+INSERT INTO parties (name, abbrev, description) VALUES ('Liberal Party', 'LP', 'Right oriented');
+INSERT INTO parties (name, abbrev, description) VALUES ('Progress Party', 'PP', 'Center oriented');
+
+INSERT INTO politicians (name, party_id, bio) VALUES ('Ion Ionel', 1, 'Ion Ionel Bio');
+INSERT INTO politicians (name, party_id, bio) VALUES ('Andrei Alin', 2, 'Sample Bio');
+INSERT INTO politicians (name, party_id, bio) VALUES ('Victor Viorel', 1, 'VV Bio');
